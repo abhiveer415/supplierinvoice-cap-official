27157dd0fd94173fbf835cd9bf9d0b86efa8bb8c
Abhilash Veerapaneni <abhi.veerapaneni@mygoconsulting.com>
2026-05-11 01:42:54 +0000

    Add health endpoint and smoke test

---
+++ srv/server.js
@@ -0,0 +1,13 @@
+const cds = require('@sap/cds')
+
+cds.on('bootstrap', app => {
+
+  app.get('/health', (req, res) => {
+    res.status(200).json({
+      status: 'UP'
+    })
+  })
+
+})
+
+module.exports = cds.server
